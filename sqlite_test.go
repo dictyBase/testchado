a3df24d0c07dd133bf0b1d919ce10046826c7643
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-01-31 13:18:09 -0500

    Added tests for retrieving backend specific schema file

--- sqlite_test.go
+++ sqlite_test.go
@@ -1,6 +1,8 @@
 package testchado
 
 import (
+    "os"
+    "path/filepath"
     "testing"
 )
 
@@ -16,5 +18,24 @@ func TestSQLiteManager(t *testing.T) {
     if dbm.dbhandler == nil {
         t.Error("should have dbhandler instance")
     }
+    if dbm.Database() == "something" {
+        t.Error("should not have any database name")
+    }
 
+    if dbm.dbsource != dbm.DataSource() {
+        t.Error("should have identical datasource")
+    }
+}
+
+func TestSQLiteSchemaPath(t *testing.T) {
+    dbm := NewSQLiteManager()
+    fpath, err := dbm.SchemaDDL()
+
+    if err != nil {
+        t.Error("Should not throw any error")
+    }
+    expath := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "dictybase", "testchado", "chado."+dbm.Driver)
+    if fpath != expath {
+        t.Error("should have returned the correct schema file path")
+    }
 }
