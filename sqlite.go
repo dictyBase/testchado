9a51e14491bb987ba93bcce0017c541bd05e510d
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-07-28 20:11:04 +0100

    Removed the import of dictybase fork of gorm Fixed the broken API after non-backward compatible upgrade of sqlx

--- sqlite.go
+++ sqlite.go
@@ -1,73 +1,74 @@
 package testchado
 
 import (
-    _ "github.com/cybersiddhu/go-sqlite3"
-    "github.com/dictybase/gorm"
-    "github.com/jmoiron/sqlx"
-    "log"
+	"log"
+
+	_ "github.com/cybersiddhu/go-sqlite3"
+	"github.com/jinzhu/gorm"
+	"github.com/jmoiron/sqlx"
 )
 
 // A type specific for sqlite backend
 type Sqlite struct {
-    *DBHelper
+	*DBHelper
 }
 
 // Get a in memory instance of sqlite DBManager
 func NewSQLiteManager() *Sqlite {
-    gm, err := gorm.Open("sqlite3", ":memory:")
-    if err != nil {
-        log.Fatal(err)
-    }
-    gm.SingularTable(true)
-    sqlx := sqlx.NewDb(gm.DB(), "sqlite3")
-    return &Sqlite{&DBHelper{dbsource: ":memory:", driver: "sqlite3", dbhandler: sqlx, gormHandler: &gm}}
+	gm, err := gorm.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Fatal(err)
+	}
+	gm.SingularTable(true)
+	sqlx := sqlx.NewDb(gm.DB(), "sqlite3")
+	return &Sqlite{&DBHelper{dbsource: ":memory:", driver: "sqlite3", dbhandler: sqlx, gormHandler: &gm}}
 }
 
 func (sqlite *Sqlite) Database() string {
-    return ""
+	return ""
 }
 
 func (sqlite *Sqlite) DropSchema() error {
-    dbh := sqlite.DBHandle()
-    type table struct{ Name string }
-    tbls := []table{}
-    err := dbh.Select(&tbls, "SELECT name FROM sqlite_master where type = ?", "table")
-    if err != nil {
-        return err
-    }
-    tx := dbh.MustBegin()
-    for _, tbl := range tbls {
-        stmt := "DROP TABLE " + tbl.Name
-        _ = tx.Execf(stmt)
-    }
-    err = tx.Commit()
-    if err != nil {
-        return err
-    }
-    sqlite.DBHelper.hasLoadedSchema = false
-    return nil
+	dbh := sqlite.DBHandle()
+	type table struct{ Name string }
+	tbls := []table{}
+	err := dbh.Select(&tbls, "SELECT name FROM sqlite_master where type = ?", "table")
+	if err != nil {
+		return err
+	}
+	tx := dbh.MustBegin()
+	for _, tbl := range tbls {
+		stmt := "DROP TABLE " + tbl.Name
+		_ = tx.MustExec(stmt)
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+	sqlite.DBHelper.hasLoadedSchema = false
+	return nil
 }
 
 func (sqlite *Sqlite) DeploySchema() error {
-    dbh := sqlite.DBHandle()
-    content, err := sqlite.SchemaDDL()
-    if err != nil {
-        return err
-    }
-    _ = dbh.Execf(content.String())
-    sqlite.DBHelper.hasLoadedSchema = true
-    return nil
+	dbh := sqlite.DBHandle()
+	content, err := sqlite.SchemaDDL()
+	if err != nil {
+		return err
+	}
+	_ = dbh.MustExec(content.String())
+	sqlite.DBHelper.hasLoadedSchema = true
+	return nil
 }
 
 func (sqlite *Sqlite) ResetSchema() error {
-    err := sqlite.DropSchema()
-    if err != nil {
-        return err
-    }
-    err = sqlite.DeploySchema()
-    if err != nil {
-        return err
-    }
-    sqlite.DBHelper.hasLoadedSchema = true
-    return nil
+	err := sqlite.DropSchema()
+	if err != nil {
+		return err
+	}
+	err = sqlite.DeploySchema()
+	if err != nil {
+		return err
+	}
+	sqlite.DBHelper.hasLoadedSchema = true
+	return nil
 }
